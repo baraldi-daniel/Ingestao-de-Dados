54c5d2ad439a7b897e9d6b6a8a7e735afa12b9a2
baraldi-daniel <108779870+baraldi-daniel@users.noreply.github.com>
2022-07-20 22:13:22 -0300

    Add files via upload

---
+++ Exercicio02/criacao_fato2_dim/mysql_create_fato2.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="MinhaSenha01",
+  database="engenhariadados"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE fato2 (CNPJ VARCHAR(255), CodigoServico VARCHAR(255), QuantidadeServicos INT);")
+
---
+++ Exercicio02/insercao_fato1e2/insert_fato2.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="MinhaSenha01",
+  database="engenhariadados"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("INSERT INTO fato2 (CNPJ, CodigoServico) SELECT CNPJ,CodigoServico FROM apidadoscoletados WHERE CNPJ!='' GROUP BY CNPJ, CodigoServico;")
+
+mycursor.execute("UPDATE fato2 SET QuantidadeServicos = 1;")
+
+mydb.commit()
+
+mydb.close()
