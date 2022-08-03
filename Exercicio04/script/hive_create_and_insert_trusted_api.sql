0ddae778c68cd1d2cd3ccd0188d31c8ef15de994
baraldi-daniel <108779870+baraldi-daniel@users.noreply.github.com>
2022-08-03 01:52:00 -0300

    Add files via upload

---
+++ Exercicio04/script/hive_create_and_insert_trusted_api.sql
@@ -0,0 +1,5 @@
+CREATE TABLE api_dados_trusted AS SELECT CONCAT(LPAD(CNPJ,8,'0')) AS CNPJ,CodigoServico,DataVigencia,Periodicidade,Servico,TipoValor,Unidade,ValorMaximo FROM default.api_dados_raw WHERE CNPJ!='';
+
+INSERT OVERWRITE DIRECTORY 's3://exercicio04/output/trusted/api/'
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ';'
+SELECT * FROM api_dados_trusted;
