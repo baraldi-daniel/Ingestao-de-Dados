13a93fa3f10b4bd06de3de72b0c981f8744fe735
baraldi-daniel <108779870+baraldi-daniel@users.noreply.github.com>
2022-08-03 01:50:56 -0300

    Delete hive_create_modelagem_fato2.sql

--- Exercicio04/script/hive_create_modelagem_fato2.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE fato2 AS SELECT CONCAT(LPAD(CNPJ,8,'0')) AS CNPJ,CodigoServico FROM default.api_dados_trusted WHERE CNPJ!='' GROUP BY CNPJ, CodigoServico;
-INSERT OVERWRITE DIRECTORY 's3://exercicio04/output/analytics/fato2/fato2'
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ';'
-SELECT * FROM fato2;
-
-CREATE TABLE dimservicos AS SELECT CodigoServico,COLLECT_SET(Servico)[0] AS Servico FROM default.api_dados_trusted GROUP BY CodigoServico;
-INSERT OVERWRITE DIRECTORY 's3://exercicio04/output/analytics/fato2/dimservicos'
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ';'
-SELECT * FROM dimservicos;
